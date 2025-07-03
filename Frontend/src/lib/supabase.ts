72b55a7d9398705f45fe90ca3e2f7e3012fdf90d
riazmd3 <riamohemed0@gmail.com>
2025-07-03 09:36:15 +0530

    Initial commit

--- Frontend/src/lib/supabase.ts
+++ Frontend/src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || '';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || '';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://agptafpjmbofrcvivbac.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFncHRhZnBqbWJvZnJjdml2YmFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk0NjgxMzYsImV4cCI6MjA2NTA0NDEzNn0.03c_6-NdE_I8J8zKPQl3rCW2CJv2_GAQswfOvvbuSQA';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
